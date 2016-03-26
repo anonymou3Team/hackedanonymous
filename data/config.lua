48f59c9672eecdf8e998a6a6eeb2fdd5c22d4dd5
anonymou3Team <anonymou3nk@gmail.com>
2016-03-26 07:14:43 +0430

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,1 +1,59 @@
-
+do local _ = {
+   admin_users = {
+                                        }
+                   ,119408525
+          "anonymou3nk"    
+                                       ,{
+                                        }
+                   ,173345972 
+          "VAMPAYER10" 
+                                        {  
+                                          , {
+   disabled_channels = {
+      ["channel#id"] = true,
+      ["channel#id"] = true,
+      ["channel#id"] = false
+                                           ,{
+   enabled_plugins = { 
+        "w",
+        "q",
+        "t",
+        "p",
+        "onservice", 
+        "inrealm", 
+        "ingroup", 
+        "inpm", 
+        "banhammer", 
+        "Boobs",
+        "Feedback", 
+        "lock_join",
+        "antilink", 
+        "antitag", 
+        "gps",
+        "auto_leave", 
+        "block", "tagall",             
+        "arabick_lock",
+        "welcome", 
+        "google", 
+        "sms", 
+        "Debian_service", 
+        "sudoers", 
+        "add_admin", 
+        "anti_spam", 
+        "add_bot", 
+        "owners", 
+        "set", 
+        "get", 
+        "broadcast",         
+        "download_medi",
+        "invite", 
+        "all", 
+        "leave_ban", 
+        "c",
+                                          ,{
+    sudo_users = {
+                      119408525 
+                                           { 
+                                               {
+return _
+end
